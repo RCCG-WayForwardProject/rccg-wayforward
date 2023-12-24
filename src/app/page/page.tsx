02550795adfaa44780b6cb83af1c34a74f51a457
royalManuel <royalmanuel62@gmail.com>
2023-12-24 09:54:49 +0100

    Fixed build error

--- src/app/page/page.tsx
+++ src/app/page/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-interface PageProps {
-  name: string;
-}
+interface PageProps {}
 
-const Page: React.FC<PageProps> = ({ name }) => {
-  return <h1>This is the custom page props, where the name is {name} </h1>;
+const Page: React.FC<PageProps> = () => {
+  return <h1>This is the custom page props, where the name is </h1>;
 };
 
 export default Page;
