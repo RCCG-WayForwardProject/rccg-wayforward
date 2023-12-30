4f76050ec30751934733c8ff46953dc90a36074c
royalManuel <royalmanuel62@gmail.com>
2023-12-30 12:58:47 +0100

    Edited Page Title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/Footer";
 import "../../styles/style.scss";
 
 export const metadata = {
-  title: "rccg-wayforwardcathedral",
+  title: "The Redeemed Christian Church Of God | Concord Church",
   description: "RCCG-Wayforward Church Website",
 };
 
