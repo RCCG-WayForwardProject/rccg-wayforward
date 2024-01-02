f3ac4865a4c1479db32b87f8060d65a8ed0c019e
royalManuel <royalmanuel62@gmail.com>
2024-01-02 13:01:08 +0100

    Added Icons and deleted Segoe UI Font

--- utils/icon.ts
+++ utils/icon.ts
@@ -1,1 +1,15 @@
-export type IconNames = "appLogo" | "mouseDown";
+export type IconNames =
+  | "appLogo"
+  | "mouseDown"
+  | "arrowDownAnimation"
+  | "cross"
+  | "worshipWordIcon"
+  | "calendar"
+  | "location"
+  | "arrowLeftCircle"
+  | "arrowRightCircle"
+  | "play"
+  | "wave"
+  | "inbox"
+  | "phone"
+  | "email";
