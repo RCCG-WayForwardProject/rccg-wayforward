9db3e9dd72f4b1f9e5c9d5bdeebc62d6e1aed5da
royalManuel <royalmanuel62@gmail.com>
2024-01-02 20:45:19 +0100

    Added About Us Page in Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import styles from "./page.module.scss";
-
 import Home from "@/components/Home";
+import AboutUs from "@/components/About";
+
+import styles from "./page.module.scss";
 
 export default function Page() {
   return (
-    <div className={styles["home"]}>
+    <div className={styles["page"]}>
       <Home />
+      <AboutUs />
     </div>
   );
 }
